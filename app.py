450c7e257cda5016d620363c6b36019b2a46de7d
rc2000123 <toametru2000@gmail.com>
2021-11-23 20:41:26 +0800

    version 1 of submission tree generator, created /submissiontree endpoint with id paramter for politics submission id, will generate tree

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import time
 import datetime
 import requests
 from requests.structures import CaseInsensitiveDict
+import treemaker
 
 app = Flask(__name__)
 
@@ -57,7 +58,6 @@ def index():
         nodeArr.append(nodeDictParent)
     json_obj['nodes'] = nodeArr
     arr.append(json_obj)
-    print(arr)
     input_file.close()
     return render_template('homepage.html', arr=arr, jjson=jjson)
 
@@ -65,6 +65,63 @@ def index():
 @app.route('/new')
 def new():
     return render_template('newhome.html')
+
+@app.route('/submissiontree')
+def sub_tree():
+    sub_id = request.args.get('id', default = 'j3ygfd', type = str)
+    print(sub_id)
+
+    input_file = open('newdealBigger232PairCount.json')
+    jjson = json.load(input_file)
+    input_file = open('less.json')
+    json_array = json.load(input_file)
+    #print(json_array)
+    json_array = treemaker.getpairlist(sub_id)
+    arr = []
+    json_obj = {}
+    json_obj['marker'] = {"radius": 13}
+    json_obj['dataLabels'] = {
+        "enabled": True,
+        "linkFormat": "",
+        "allowOverlap": True,
+        "style": {"textOutline": False}
+    }
+    pairArr = []
+    for obj in json_array:
+        json_pair = []
+        for key in obj.keys():
+            json_pair.append(key)
+            json_pair.append(obj[key]['parentAuthor'])
+            break
+            # print(key)
+            # print(obj[key]['parentAuthor'])
+        pairArr.append(json_pair)
+    json_obj['data'] = pairArr
+    nodeArr = []
+    colorDict = {}
+    colorDict['dem'] = "#0000ff"
+    colorDict['rep'] = "#E8544E"
+    colorDict['error'] = "#808080"
+    for obj in json_array:
+        nodeDictKid = {}
+        nodeDictParent = {}
+        for key in obj.keys():
+            nodeDictKid['id'] = key
+            nodeDictParent['id'] = obj[key]['parentAuthor']
+            nodeDictKid['marker'] = {"radius": 20}
+            nodeDictParent['marker'] = {"radius": 20}
+            nodeDictKid['color'] = colorDict[obj[key]['childLeaning']]
+            nodeDictParent['color'] = colorDict[obj[key]['parentLeaning']]
+            break
+        nodeArr.append(nodeDictKid)
+        nodeArr.append(nodeDictParent)
+    json_obj['nodes'] = nodeArr
+    arr.append(json_obj)
+    #print(arr)
+    input_file.close()
+    #print(json.dumps(arr,indent = 2, separators=(',', ': ')))
+    #print(json.dumps(jjson,indent = 2, separators=(',', ': ')))
+    return render_template('homepage.html', arr=arr, jjson=None)
 
 
 if __name__ == '__main__':
--- treemaker.py
+++ treemaker.py
@@ -37,32 +37,57 @@ def execute_SQL(insert_stmt):
 def subtree(sub_id):
     connect()
     subtreeroot = {}
-    sub_id = 't3_'+sub_id
     comment_id_list = execute_SQL(
-        "select id from politicscom2020 where parent_id = '{sub}'".format(sub=sub_id))
+        "select id from politicscom2020 where parent_id = '{sub}'".format(sub='t3_'+sub_id))
     comment_id_list = [x for (x,) in comment_id_list]
+    sub_leaning = execute_SQL("select leaning from politicsuserleaning,politicssub2020 where politicssub2020.id = '{sub}' and politicsuserleaning.author = politicssub2020.author".format(sub=sub_id))[0][0]
+    #print(sub_leaning)
 
     for comment_id in comment_id_list:
-        subtreeroot[comment_id] = comchild(comment_id)
+        com_leaning = execute_SQL("select leaning from politicsuserleaning,politicscom2020 where politicscom2020.id = '{com}' and politicsuserleaning.author = politicscom2020.author".format(com=comment_id))[0][0]
+        dict_obj = {}
+        child_dict_obj = {}
+        child_dict_obj["parentAuthor"] = sub_id
+        child_dict_obj["parentLeaning"] = sub_leaning
+        child_dict_obj["childLeaning"] = com_leaning
+        dict_obj[comment_id] = child_dict_obj
+        pair_list.append(dict_obj)
+        subtreeroot[comment_id] = comchild(comment_id,com_leaning)
     return subtreeroot
 
 
-def comchild(com_id):
+def comchild(com_id,com_leaning):
     comtreeroot = {}
-    com_id = 't1_'+com_id
     comment_id_list = execute_SQL(
-        "select id from politicscom2020 where parent_id = '{com}'".format(com=com_id))
+        "select id from politicscom2020 where parent_id = '{com}'".format(com='t1_'+com_id))
     comment_id_list = [x for (x,) in comment_id_list]
-
+    
     for comment_id in comment_id_list:
-        comtreeroot[comment_id] = comchild(comment_id)
+        child_com_leaning = execute_SQL("select leaning from politicsuserleaning,politicscom2020 where politicscom2020.id = '{com}' and politicsuserleaning.author = politicscom2020.author".format(com=comment_id))[0][0]
+        dict_obj = {}
+        child_dict_obj = {}
+        child_dict_obj["parentAuthor"] = com_id
+        child_dict_obj["parentLeaning"] = com_leaning
+        child_dict_obj["childLeaning"] = child_com_leaning
+        dict_obj[comment_id] = child_dict_obj
+        pair_list.append(dict_obj)
+        comtreeroot[comment_id] = comchild(comment_id,child_com_leaning)
+
     return comtreeroot
 
-
+pair_list = []
 def main():
     connect()
-    print(json.dumps(subtree('j3ygfd'), indent=4, sort_keys=True))
+    print(getpairlist('j3ygfd'))
+    #print(json.dumps(subtree('j3ygfd'), indent=4, sort_keys=True))
+    #print(pair_list)
 
+def getpairlist(sub_id):
+    global pair_list
+    pair_list=[]
+    json.dumps(subtree(sub_id), indent=4, sort_keys=True)
+    print(pair_list)
+    return pair_list
 
 if __name__ == '__main__':
     main()
